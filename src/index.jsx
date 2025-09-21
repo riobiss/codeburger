a0f3c79e34ed5337606d6d343b4a432c9972e03d
Riobiss <riobisofcl@gmail.com>
2025-09-21 15:45:26 -0300

    base front

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Login from "./containers/Login/index"
+import GlobalStyles from "./styles/globalStyles"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+  <>
+    <Login />
+    <GlobalStyles />
+  </>
+)
+
+const ola = 1
