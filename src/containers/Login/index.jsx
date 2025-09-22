ffdaf29c3ab202b2a82d53257a682c6c89852b9d
Riobiss <riobisofcl@gmail.com>
2025-09-21 21:01:43 -0300

    feat: criar tela de login

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,9 +1,32 @@
 import react from "react"
+import LoginImg from "../../assets/login-image.svg"
+import Logo from "../../assets/logo.svg"
+import {
+  Container,
+  LoginImage,
+  ContainerItens,
+  Label,
+  Input,
+  Button,
+  SignInLink,
+} from "./styles"
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LoginImage src={LoginImg} />
+      <ContainerItens>
+        <img src={Logo} alt="logo" />
+        <h1>Login</h1>
+        <Label>Email</Label>
+        <Input />
+        <Label>Senha</Label>
+        <Input />
+        <Button>Sign in</Button>
+        <SignInLink>
+          Não possui conta? <a>Sign Up</a>
+        </SignInLink>
+      </ContainerItens>
+    </Container>
   )
 }
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+import Background from "../../assets/background.svg"
+
+export const Container = styled.div`
+height: 100dvh;
+width: 100dvw;
+background: url("${Background}") no-repeat center/cover;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+export const LoginImage = styled.img`
+height: 70%;
+`
+
+export const ContainerItens = styled.div`
+background-color: #373737;
+box-shadow: 0px 4px 15px rgba(74, 144,226, 0.24);
+border-radius: 0 10px 10px 0;
+height: 70%;
+padding: 25px 75px; 
+display: flex;
+flex-direction: column;
+justify-content: center;
+
+ h1 {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 28px;
+    color: white;
+    margin-top: 100px;
+ }
+`
+
+export const Label = styled.p`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    color: white;
+    margin-top: 28px;
+    margin-bottom: 5px;
+
+`
+
+export const Input = styled.input`
+width: 391.42px;
+height: 38.32px;
+background-color: white;
+box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
+border-radius: 5px;
+border: none;
+text-indent: 10px;
+`
+
+export const Button = styled.button`
+width: 182.81px;
+height: 36.13px;
+background: #9758a6;
+border-radius: 20px;
+border:none;
+cursor: pointer;
+color: #eeeeee;
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 19px;
+text-align:center;
+margin-top: 75px;
+margin-bottom: 25px;
+
+&:hover {
+    opacity: 0.8;
+}
+
+&:active {
+    opacity: 0.6;
+}
+`
+
+export const SignInLink = styled.p`
+font-style: normal;
+font-weight: 300    ;
+font-size: 14px;
+line-height:16px;
+color: white;
+a {
+    cursor: pointer;
+ text-decoration: underline;
+}
+`
