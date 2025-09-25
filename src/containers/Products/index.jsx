6e2392627926df2055663ec890f29d3be80859ae
Riobiss <riobisofcl@gmail.com>
2025-09-25 12:27:05 -0300

    feat: categorias e efeito na selecionada

--- src/containers/Products/index.jsx
+++ src/containers/Products/index.jsx
@@ -1,11 +1,48 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+
+import api from "../../services/api.js"
 import ProductsLogo from "../../assets/products-logo.svg"
-import {Container, ProductImg} from "./styles.jsx"
+import {
+  Container,
+  ProductImg,
+  CategoryButton,
+  CategoriesMenu,
+} from "./styles.jsx"
 
 export default function Products() {
+  const [categories, setCategories] = useState([])
+  const [activeCategory, setActiveCategory] = useState(0)
+
+  useEffect(() => {
+    async function LoadCategories() {
+      const {data} = await api.get("categories")
+      const newCategories = [{id: 0, name: "Todos"}, ...data]
+      setCategories(newCategories)
+    }
+
+    LoadCategories()
+  }, [])
+
   return (
     <Container>
       <ProductImg src={ProductsLogo} alt="Logo do Home" />
+      <CategoriesMenu>
+        {categories &&
+          categories.map(category => {
+            return (
+              <CategoryButton
+                key={category.id}
+                $isActiveCategory={activeCategory === category.id}
+                type="button"
+                onClick={() => {
+                  setActiveCategory(category.id)
+                }}
+              >
+                {category.name}
+              </CategoryButton>
+            )
+          })}
+      </CategoriesMenu>
     </Container>
   )
 }
--- src/containers/Products/styles.jsx
+++ src/containers/Products/styles.jsx
@@ -5,3 +5,19 @@ export const Container = styled.div``
 export const ProductImg = styled.img`
   width: 100%;
 `
+export const CategoryButton = styled.button`
+  cursor: pointer;
+  background: none;
+  border: none;
+  color: ${props => (props.$isActiveCategory ? "#9758A6" : "#9a9a9d")};
+  font-size: 17px;
+  line-height: 30px;
+  border-bottom: ${props =>
+    props.$isActiveCategory && "2px solid #9758A6"};
+    padding-bottom: 6px;
+`
+export const CategoriesMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+`
