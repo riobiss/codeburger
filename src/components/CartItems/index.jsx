117da996cf422879bb3e1aa06353d483ee534402
Riobiss <riobisofcl@gmail.com>
2025-09-26 16:56:25 -0300

    feat: mostrar produtos no carrinho

--- src/components/CartItems/index.jsx
+++ src/components/CartItems/index.jsx
@@ -1,19 +1,20 @@
 import React from "react"
 import {useCart} from "../../hooks/CartContext.jsx"
-import {Container, Header, Body} from "./styles.jsx"
-import { formatCurrency } from "../../utils/formatCurrency.js"
+import {Container, Header, Body, EmptyCart} from "./styles.jsx"
+import {formatCurrency} from "../../utils/formatCurrency.js"
 export function CartItems() {
   const {cartProducts} = useCart()
   return (
     <Container>
       <Header>
         <p>Itens</p>
-        <p>Preço</p>
+          <p>Preço</p>
         <p>Quantidade</p>
         <p>Total</p>
       </Header>
 
-      {cartProducts.map(product => {
+      { cartProducts.length > 0 ? 
+      cartProducts.map(product => {
         return (
           <Body key={product.id}>
             <img src={product.url} alt={product.name} />
@@ -23,7 +24,14 @@ export function CartItems() {
             <p>{formatCurrency(product.quantity * product.price)}</p>
           </Body>
         )
-      })}
+      })
+      : (
+      <EmptyCart>
+           <p>Carinho vazio</p>
+      </EmptyCart>
+      )
+
+    }
     </Container>
   )
 }
--- src/components/CartItems/styles.jsx
+++ src/components/CartItems/styles.jsx
@@ -1,13 +1,40 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-background-color: #e5e5e5;
-min-height: 100vh;
+  background-color: white;
+  padding: 10px;
+  border-radius: 20px;
+  box-shadow: 0 10px 40px rgba(0, 0, 0, 0.03);
+  width: 60%;
 `
 
-export const Header = styled.div `
-
-
+export const Header = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr 1fr;
+  padding: 10px;
+  border-bottom: 1px solid #81818186;
+  p {
+    font-size: 16px;
+    color: grey;
+  }
 `
-export const Body = styled.div `
+export const Body = styled.div`
+margin-top: 10px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 10px 15px;
+  img {
+    border-radius: 10px;
+    width: 120px;
+  }
+  p {
+    font-size: 16px;
+  }
+`
+export const EmptyCart = styled.div`
+  p {
+    font-weight: bold;
+    text-align: center;
+    padding: 20px;
+  }
 `
