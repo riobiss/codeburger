376512685798733a4fa92af1f16cf0c9adc44e62
Riobiss <riobisofcl@gmail.com>
2025-09-30 14:36:46 -0300

    chore: mudança na url

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -11,7 +11,7 @@ class Product extends Model {
         url: {
           type: Sequelize.VIRTUAL, // campo virtual, não existe na tabela
           get() {
-            return `http://localhost:3000/product-file/${this.path}`
+            return `http://localhost:3001/product-file/${this.path}`
           },
         },
       },
