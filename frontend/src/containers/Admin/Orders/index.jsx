6d0d5dbfcfb25a920aed7e61fbc5f81b3cde502b
Riobiss <riobisofc@gmail.com>
2025-12-05 16:34:28 -0300

    add orders to admin

---
+++ frontend/src/containers/Admin/Orders/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles"
+
+export function Order() {
+  return (
+    <Container>
+      <h1>Ordens</h1>
+    </Container>
+  )
+}
---
+++ frontend/src/containers/Admin/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles"
+import { Order } from "./Orders"
+export function Admin() {
+  return (
+    <Container>
+      <h1>Admin</h1>
+      <Order />
+    </Container>
+  )
+}
