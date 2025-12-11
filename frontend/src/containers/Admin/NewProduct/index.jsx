c918fb6e97e20edc72a047c4467fcb89ae4a74a5
Riobiss <riobisofc@gmail.com>
2025-12-11 11:36:28 -0300

    add useForm for create NewProduct and initilizad

--- frontend/src/containers/Admin/NewProduct/index.jsx
+++ frontend/src/containers/Admin/NewProduct/index.jsx
@@ -1,6 +1,10 @@
-import { Container } from "./styles.jsx"
+import { Container, Input, Label } from "./styles.jsx"
+import { Button } from "../../../components/Button"
+import ReactSelect from "react-select"
 import api from "../../../services/api.js"
 import { useEffect } from "react"
+import { useForm } from "react-hook-form"
+
 export default function ListProducts() {
   useEffect(() => {
     async function loadOrders() {
@@ -8,5 +12,26 @@ export default function ListProducts() {
     }
     loadOrders()
   }, [])
-  return (<Container> ok</Container>)
+    const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  return (
+    <Container>
+      <form noValidate>
+        {/* calaboca html */}
+        <Label>Nome</Label>
+        <Input type="text" {...register("name")} />
+        <Label>Preço</Label>
+        <Input type="number" {...register("name")} />
+        <Label>Upload da imagem</Label>
+        <Input type="file" accept="image/png, image/jpeg" />
+        <ReactSelect />
+        <Button>Adicionar produto </Button>
+      </form>
+    </Container>
+  )
 }
