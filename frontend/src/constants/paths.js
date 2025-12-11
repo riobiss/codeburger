fde1f3e8d79b174e2f7e58330945074641d81024
Riobiss <riobisofc@gmail.com>
2025-12-11 10:19:30 -0300

    add paths for app

---
+++ frontend/src/constants/paths.js
@@ -0,0 +1,12 @@
+const paths = {
+  Home: "/",
+  Login: "/login",
+  Register: "/cadastro",
+  Products: "/produtos",
+  Cart: "/carrinho",
+  Order: "/pedidos",
+  ListProducts: "/listar-produtos",
+  EditProduct: "/editar-produto",
+  NewProduct: "/novo-produto",
+}
+export default paths
