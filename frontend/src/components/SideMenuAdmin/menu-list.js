83c83f1647d5654ec96d5d43703671816e73abea
Riobiss <riobisofc@gmail.com>
2025-12-11 10:21:44 -0300

    add switch location for order and listProducts

--- frontend/src/components/SideMenuAdmin/menu-list.js
+++ frontend/src/components/SideMenuAdmin/menu-list.js
@@ -1,4 +1,18 @@
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';const listLinks = [
-  { id: 1, label: "Pedidos", link: "/pedidos", icon: ShoppingBagOutlinedIcon },
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import paths from "../../constants/paths"
+const listLinks = [
+  {
+    id: 1,
+    label: "Pedidos",
+    link: paths.Order,
+    icon: ShoppingBagOutlinedIcon,
+  },
+  {
+    id: 2,
+    label: "Listar Produtos",
+    link: paths.ListProducts,
+    icon: ShoppingCartIcon,
+  },
 ]
 export default listLinks
--- frontend/src/containers/Admin/index.jsx
+++ frontend/src/containers/Admin/index.jsx
@@ -2,13 +2,16 @@ import { Container, ContainerItems } from "./styles"
 import SideMenuAdmin from "../../components/SideMenuAdmin"
 import ListProducts from "./ListProducts"
 import Order from "./Orders"
+import { useLocation } from "react-router-dom"
+import paths from "../../constants/paths"
 export function Admin() {
+  const location = useLocation().pathname
   return (
     <Container>
       <SideMenuAdmin />
       <ContainerItems>
-    {/*   <Order /> */}
-      <ListProducts />
+        {location === paths.Order && <Order/>}
+        {location === paths.ListProducts && <ListProducts/>}
       </ContainerItems>
     </Container>
   )
